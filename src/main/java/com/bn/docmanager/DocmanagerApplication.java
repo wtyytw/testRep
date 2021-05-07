9ae2eabbf02e8379134fef355ccfda4c9a5ad188
wty <ytwytw200601@qq.com>
2021-05-07 15:29:37 +0800

    ceshi2

--- src/main/java/com/bn/docmanager/DocmanagerApplication.java
+++ src/main/java/com/bn/docmanager/DocmanagerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DocmanagerApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DocmanagerApplication.class, args);
 	}
